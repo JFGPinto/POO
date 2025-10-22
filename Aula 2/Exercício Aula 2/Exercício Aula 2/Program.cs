05b86d9a7666a1a624a6105d1a8dde16c837c0aa
JFGPinto <jfgpinto95@gmail.com>
2025-10-22 16:05:31 +0100

    Aula 1 - Assinatura, region e introdução ao C#

---
+++ Aula 2/Exercício Aula 2/Exercício Aula 2/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício_Aula_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Zeros de uma equação do se2gundo grau. É necessário saber o valor de a, b e c.
+
+            //Definir as variáveis.
+            int a;
+            int b;
+            int c;
+            double x1;
+            double x2;
+
+            //Atribuir valores às variáveis.
+
+            a = 0; b = 4; c = 3;
+
+            //Calcular os Zeros (valores de x1 e x2).
+
+            x1 = (-b + Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a); //Fórmula de Bhaskara: -b ± √(b² - 4ac) / 2a
+            //Math.Pow(Number1, Number2) - Potências no C# são definidas assim. Number1 é a base e Number2 é o expoente.
+            //Math.sqrt(Number) - Raiz quadrada no C# é definida assim. Number é o número que você quer a raiz quadrada.
+
+            x2 = (-b - Math.Sqrt(Math.Pow(b, 2) - 4 * a * c)) / (2 * a);
+
+            //Mostrar os resultados no console.
+
+            if (a == 0) {                
+                Console.WriteLine("O valor de 'a' não pode ser 0.");
+            }
+
+            else
+            {
+                Console.WriteLine("Zeros da equação: " + x1 + " e " + x2);
+            }
+            
+            Console.WriteLine(" "); //Espaço em branco no console.
+
+            //Conversão de temperatura de Fahrenheit para Celsius.
+
+            //Definir as variáveis.
+
+            float fahrenheit;
+            float celsius;
+
+            //Atribuir valores às variáveis.
+
+            fahrenheit = 77;
+
+            //Calcular a conversão.
+
+            celsius = (fahrenheit - 32) * 5 / 9;
+
+            //Mostrar o resultado no console.
+
+            Console.WriteLine("Temperatura em Celsius: " + celsius + "ºC");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aula 2/Trabalho de casa Aula 2/Trabalho de casa Aula 2/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabalho_de_casa_Aula_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1. Declare variáveis para armazenar: nome (string), idade (int) e altura(double) se é estudante (bool). Atribua valores e mostre-os na consola.
+            string nome;
+            int idade;
+            float altura;
+            bool estudante;
+            nome = "João";
+            idade = 30;
+            altura = 1.83f;
+            estudante = true;
+            Console.WriteLine("Nome: " + nome);
+            Console.WriteLine("Idade: " + idade);
+            Console.WriteLine("Altura: " + altura);
+            Console.WriteLine("Estudante: " + estudante);
+            Console.ReadLine(); 
+            */
+
+            /*2. Troque os valores de duas variáveis inteiras a e b (sem usar uma terceira variável auxiliar).
+            //Declarar variáveis
+            int a, b;
+
+            //Atribuir valores
+            a = 5;
+            b = 7;
+
+            Console.WriteLine("Antes da troca: ");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.ReadLine();
+
+            //Aplicar lógica para trocar valores
+            a = a + b; // a = 12
+            b = a - b; // a= 12; b = 5
+            a = a - b; // a = 7; b = 5
+
+            Console.WriteLine("Após a troca: ");
+            Console.WriteLine("a = " + a);
+            Console.WriteLine("b = " + b);
+            Console.ReadLine();
+            */
+
+            /*3.
+            string nome1 = "João Pinto";
+            int dia, mes, ano;
+            Console.WriteLine("Digite o nome: ");
+            nome1 = Console.ReadLine();
+
+            Console.WriteLine("Digite o Dia: ");
+            dia = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Mês: ");
+            mes = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o Ano: ");
+            ano = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Boa tarde, " + nome1 + " nascido em " + dia + "/" + mes + "/" + ano);
+            */
+
+            /*4.
+            string nome1;
+            string nome2;
+            uint idade1;
+            uint idade2;
+            nome1 = "Maria";
+            nome2 = "Jorge";
+            idade1 = 54;
+            idade2 = 54;
+            Console.WriteLine("Nome: " + nome1 + ", Idade: " + idade1);
+            Console.WriteLine("Nome: " + nome2 + ", Idade: " + idade2);
+            Console.ReadLine();
+            */
+
+            /*5.
+            string nome1;
+            string nome2;
+            float salario1;
+            float salario2;
+            nome1 = "Ana";
+            nome2 = "Carlos";
+            salario1 = 1200.50f;
+            salario2 = 1500.75f;
+            Console.WriteLine("Nome: " + nome1 + ", Salário: " + salario1);
+            Console.WriteLine("Nome: " + nome2 + ", Salário: " + salario2);
+            Console.ReadLine();
+            */
+
+            /*6
+            uint codigoProduto = 674135;
+            uint stock = 150;
+            double precoProduto = 29.99;
+            Console.WriteLine("Código do Produto: " + codigoProduto + ", Quantidade: " + stock + ", Preço por peça: " + precoProduto);
+            Console.ReadLine();
+            */
+
+            //7. uint quer dizer que o valor só pode ser positivo. int pode assumir valores negativos.
+            /*8.hey there ^
+            continua*/
+            //9.
+
+
+
+
+
+        }
+    }
+}
